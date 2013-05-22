17b53bf263aae378ef49cbcc8f0bbba5a965f252
animeshkbhadra <animesh.bhadra09@gmail.com>
2013-05-23 00:34:35 +0530

    parseFloat() Example

---
+++ src/chapter03/NumberExample08.js
@@ -0,0 +1,6 @@
+alert("parseFloat('1234Blue') is = " +parseFloat('1234Blue') );
+alert("parseFloat('0xA') is = " +parseFloat('0xA') );
+alert("parseFloat('22.5') is = " +parseFloat('22.5') );
+alert("parseFloat('22.34.5') is = " +parseFloat('22.34.5') );
+alert("parseFloat('0908.5') is = " +parseFloat('0908.5') );
+alert("parseFloat('3.125e7') is = " +parseFloat('3.125e7') );
